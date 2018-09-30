1a3f3a5e5557a497592b23d6107d10264bdfed9e
isulley77 <isulley@stuy.edu>
2018-09-30 01:57:41 -0400

    Array Demo 0-1

---
+++ Desktop/APCS/ArrayDemo.java
@@ -0,0 +1,81 @@
+public class ArrayDemo {
+
+
+    public static void main(String[] args) {
+
+
+    //Test Cases
+    int[] testArray = {1,2,3,4};
+    printArray(testArray);
+    
+    int[][] test2DArray = new int[][]{
+        {1,2,3,0},
+        {5,0,7,8},
+        {9,10,0,12},
+        };
+    printArray(test2DArray);
+    countZeros2D(test2DArray);
+}
+
+    
+    
+    
+    //Prints a 1D Array
+    public static void printArray(int[] ary){
+        
+
+        
+        System.out.print("{");
+        for(int i = 0; i < ary.length; i++){
+            System.out.print(ary[i]);
+            System.out.print(",");
+        }
+         System.out.print("\b}");
+         System.out.println();
+    }
+
+    
+    public static void printArray(int[][] ary){
+    
+        int row = ary.length;
+        int col = ary[0].length;
+        System.out.println("{");
+        
+        for(int i = 0; i < row; i++){
+            
+            System.out.print("{");
+            
+            for(int j = 0; j < col; j++){
+            
+            System.out.print(ary[i][j]);
+            System.out.print(",");
+            }
+            System.out.print("\b");
+            System.out.println("},");
+            
+        }
+        System.out.println("}");
+    }
+
+    public static int countZeros2D(int[][] nums){
+        
+        int row = nums.length;
+        int col = nums[0].length;
+        int counter = 0;
+        
+        for(int i = 0; i < row; i++){
+            
+            for(int j = 0; j < col; j++){
+                
+                if(nums[i][j] == 0){
+                    counter++;
+                }
+            }
+       }
+       System.out.println(counter);
+       return counter;
+  
+    }
+    
+}
+
